284c172db3db4b2dee8c710e1fbd4a90ad21180d
Jeff Schofield <jeff@jeffschofield.com>
2022-06-27 16:55:16 -0400

    feat: Add Vite plugin

---
+++ src/vite.ts
@@ -0,0 +1,16 @@
+﻿import type { Plugin } from 'vite'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import { colorSuitePlugin } from 'tailwindcss-color-suite'
+import { resolve } from 'path'
+
+export default function uiPlugin({ colorSuite = false }:{ colorSuite?:boolean|Parameters<typeof colorSuitePlugin>[0] } = {}):Plugin[] {
+  const SVGIcons = createSvgIconsPlugin({
+    iconDirs: [resolve(__dirname, '../assets/icons')],
+    customDomId: 'slui-svg-icons'
+  })
+
+	const plugins = [SVGIcons]
+	if (colorSuite) plugins.push(colorSuitePlugin(typeof colorSuite == 'object' ? colorSuite : {}))
+
+  return plugins
+}
---
+++ vite.config.vite.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    emptyOutDir: false,
+    lib: {
+      entry: resolve(__dirname, 'src/vite.ts'),
+      name: 'SLUI-Vite',
+      fileName: (format) => `vite.${format}.js`
+    },
+    rollupOptions: {
+      external: ['tailwindcss-color-suite', 'vite-plugin-svg-icons', 'path'],
+      output: {
+        globals: { 'tailwindcss-color-suite': 'tailwindcssColorSuite' }
+      }
+    }
+  }
+})
