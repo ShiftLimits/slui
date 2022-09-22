43e17ea13575bc391b699c2ab04cad462434259e
Jeff Schofield <jeff@jeffschofield.com>
2022-09-22 18:51:05 -0400

    feat: Add syncModelValue utility

---
+++ src/utils.ts
@@ -0,0 +1,31 @@
+﻿import { nextTick, ref, watch } from 'vue'
+
+export function syncModelValue(
+  props: Readonly<any>,
+  emit: (event: any, payload: any) => void,
+  prop = 'modelValue'
+) {
+  const value = ref(props[prop])
+
+  const flush = 'sync'
+  let syncing = false
+  watch(
+    () => props[prop],
+    new_value => {
+      syncing = true
+      value.value = new_value
+      nextTick(() => (syncing = false))
+    },
+    { flush, immediate: true }
+  )
+  watch(
+    value,
+    new_value => {
+      if (syncing) syncing = false
+      else emit(`update:${prop}`, new_value)
+    },
+    { flush, immediate: false }
+  )
+
+  return value
+}
