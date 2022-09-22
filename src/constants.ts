406427120107e639b05949d3c7ff077e71a22df8
Jeff Schofield <jeff@jeffschofield.com>
2022-09-22 18:49:56 -0400

    feat: Add types and constants for component sizes and colors

---
+++ src/constants.ts
@@ -0,0 +1,36 @@
+﻿import { ComponentColors, ComponentSize } from './types'
+
+export const component_colors: ComponentColors[] = [
+  'primary',
+  'secondary',
+  'neutral',
+  'info',
+  'success',
+  'warning',
+  'alert',
+  'link',
+  'slate',
+  'gray',
+  'gray-neutral',
+  'zinc',
+  'stone',
+  'red',
+  'orange',
+  'amber',
+  'yellow',
+  'lime',
+  'green',
+  'emerald',
+  'teal',
+  'cyan',
+  'sky',
+  'blue',
+  'indigo',
+  'violet',
+  'purple',
+  'fuchsia',
+  'pink',
+  'rose'
+]
+
+export const component_sizes: ComponentSize[] = ['xs', 'sm', 'base', 'lg']
